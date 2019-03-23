34781a4284485ff73b6706261a2e06cbe2c6ba7f
Alfred Tso <alfredtso@gmail.com>
2019-03-23 21:50:01 +0800

    using 1 PQ

--- Algo1Assignment4/src/Solver.java
+++ Algo1Assignment4/src/Solver.java
@@ -9,8 +9,7 @@ import java.util.Stack;
 public class Solver {
 
     private MinPQ<Manhattan> pq;
-    private Queue<Board> sol;
-    private Queue<Board> twinsSol;
+    private Stack<Board> sol;
     private boolean solvable;
     private int moves;
 
@@ -18,27 +17,43 @@ public class Solver {
         // TODO: not sure if all 3 is needed
         private int priority;
         private int manhattan;
+        private int moves;
         private final Board board;
+        private final Manhattan previousNode;
         private final boolean valid;
         private boolean twins;
 
         // TODO: need to include a twin and solve
 
-        Manhattan(Board board, Board previousBoard, int moves, boolean twins) {
+        Manhattan(Board board, Manhattan previousNode, int moves, boolean twins) {
             this.board = board;
-            this.valid = !board.equals(previousBoard);
+            this.previousNode = previousNode;
+            if (previousNode == null) {
+                this.valid = true;
+            } else {
+                this.valid = !board.equals(previousNode.board);
+            }
             this.manhattan = board.manhattan();
             this.priority = manhattan + moves;
             this.twins = twins;
+            this.moves = moves;
         }
 
         public int compareTo(Manhattan that) {
             // breaking ties case using hamming distance
             if (this.priority == that.priority) {
-                return this.board.hamming() - that.board.hamming();
+                if (this.manhattan == that.manhattan) {
+                    return this.board.hamming() - that.board.hamming();
+                } else {
+                    return this.manhattan - that.manhattan;
+                }
             }
             // < 0 meaning lower priority, therefore got removed earlier
             return this.priority - that.priority;
+        }
+
+        private Manhattan previousNode() {
+            return previousNode;
         }
 
         private boolean isValid() {
@@ -52,49 +67,41 @@ public class Solver {
         // TODO: check initial validity
         // initialize
         pq = new MinPQ<>();
-        sol = new Queue<>();
-        twinsSol = new Queue<>();
+        sol = new Stack<>();
 
         // insert initial search node
         Board currentBoard = initial;
         Board currentTwins = initial.twin();
 
-    // if twin is solvable ( have a goal board) then the initial one is not solvable
-        sol.enqueue(initial);
-        twinsSol.enqueue(currentTwins); // initial.twins?
-        this.moves = 0;
         // use boolean instead ?
-        int twinTurn = 0;
+        boolean twinTurn = false;
 
         // for temp storage
         Stack<Manhattan> temp = new Stack<>();
-        Manhattan currentNode;
-        Manhattan currentNodeTwins;
+        Manhattan minNode = new Manhattan(initial, null, 0, false);
+        Manhattan minNodeTwin = new Manhattan(initial.twin(), null, 0, true);
 
-        while (!currentBoard.isGoal()) {
-
-            //update moves
-            if (twinTurn%2 == 0) { this.moves++; }
+        while (!currentBoard.isGoal() && !currentTwins.isGoal()) {
 
             // add first loop of search nodes
             // todo; this is initial queue
             Manhattan localval;
             Iterable<Board> debugWatch;
-            if (twinTurn%2 == 0) {
+            if (twinTurn == false) {
                 debugWatch = currentBoard.neighbors();
                 for (Board b : debugWatch) {
-                    localval = new Manhattan(b, currentBoard, moves, false);
+                    localval = new Manhattan(b, minNode, minNode.moves+1, false);
                     if (localval.isValid()) {
                         pq.insert(localval);
                     }
                 }
 
                 // temp stack to store non twins node
-                currentNode = pq.delMin();
+                minNode = pq.delMin();
                 // while currentNode is twins
-                while (currentNode.twins) {
-                    temp.push(currentNode);
-                    currentNode = pq.delMin();
+                while (minNode.twins) {
+                    temp.push(minNode);
+                    minNode = pq.delMin();
                 }
 
                 // put it back, empty it
@@ -103,8 +110,10 @@ public class Solver {
                 }
 
                 // update currentTwin
-                currentBoard = currentNode.board;
-                sol.enqueue(currentBoard);
+                currentBoard = minNode.board;
+
+                // TwinTue
+                twinTurn = true;
 
                 // todo: assertion should be turn off later
                 assert temp.isEmpty();
@@ -113,17 +122,18 @@ public class Solver {
             else {
                 debugWatch = currentTwins.neighbors();
                 for (Board b : debugWatch) {
-                    localval = new Manhattan(b, currentTwins, moves, true);
+                    localval = new Manhattan(b, minNodeTwin, minNodeTwin.moves+1, true);
                     if (localval.isValid()) {
                         pq.insert(localval);
                     }
                 }
 
                 // temp stack to store non twins node
-                currentNodeTwins = pq.delMin();
-                while (!currentNodeTwins.twins) {
-                    temp.push(currentNodeTwins);
-                    currentNodeTwins = pq.delMin();
+                minNodeTwin = pq.delMin();
+                // while the node is not twins
+                while (!minNodeTwin.twins) {
+                    temp.push(minNodeTwin);
+                    minNodeTwin = pq.delMin();
                 }
 
                 // put it back
@@ -132,8 +142,8 @@ public class Solver {
                 }
 
                 // update currentTwin
-                currentTwins = currentNodeTwins.board;
-                twinsSol.enqueue(currentTwins);
+                currentTwins = minNodeTwin.board;
+                twinTurn = false;
 
                 //todo: assertion
                 assert temp.isEmpty();
@@ -141,11 +151,20 @@ public class Solver {
 
             // update twinTurn, meaning if twinTurn odd num is initial even is twins
             //todo: increment might overflow the int?
-            twinTurn++;
         }
-        solvable = (twinTurn%2 == 1);
+        // because before twinturn is true
+        solvable = currentBoard.isGoal();
         System.out.println(isSolvable());
 
+        // todo: chase has problem
+        // chase back from currentboard/currentnode
+        if (isSolvable()) {
+            while (minNode != null) {
+                sol.push(minNode.board);
+                moves++;
+                minNode = minNode.previousNode();
+            }
+        }
     }
 
     // is the initial board solvable?
@@ -157,15 +176,19 @@ public class Solver {
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
         if (isSolvable()) {
-            return moves;
+            return moves-1;
         }
         else { return -1;}
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
+        Queue<Board> result = new Queue<>();
         if (isSolvable()) {
-            return sol;
+            while (!sol.isEmpty()) {
+                result.enqueue(sol.pop());
+            }
+            return result;
         }
         else { return null; }
     }
@@ -183,9 +206,6 @@ public class Solver {
 
         // solve the puzzle
         Solver solver = new Solver(initial);
-
-        for (Board board : solver.solution())
-            StdOut.println(board);
 
 //         print solution to standard output
         if (!solver.isSolvable())
